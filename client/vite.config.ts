b03c4984571d87b6b70f309557d74f83a28e93f0
lonefaceit228-hash <lonefaceit228@gmail.com>
2026-02-10 02:09:36 +0100

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
-  root: ".",
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
+  plugins: [react()]
 });
